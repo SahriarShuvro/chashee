7580f02373f8f662c99a305568db1f44f3a8b764
SahriarShuvro <8199shuvro@gmail.com>
2024-03-25 02:21:34 +0600

    feat: add from data in console

--- client/src/components/user/Form/Post.jsx
+++ client/src/components/user/Form/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import {
     Button,
@@ -24,9 +25,24 @@ const normFile = (e) => {
     return e?.fileList;
 };
 const FormDisabledDemo = () => {
+    const [plantingTime, setPlantingTime] = useState(null);
+
+    const handlePlantingTimeChange = (dates) => {
+        if (dates && dates.length === 2) {
+            const start = moment(dates[0]).toISOString();
+            const end = moment(dates[1]).toISOString();
+            setPlantingTime({ start, end });
+        } else {
+            setPlantingTime(null);
+        }
+    };
+    const onFinish = (e) => {
+        console.log(e);
+    }
     return (
         <>
             <Form
+                onFinish={onFinish}
                 layout="vertical"
             >
                 <Form.Item valuePropName="fileList" getValueFromEvent={normFile}>
@@ -50,67 +66,68 @@ const FormDisabledDemo = () => {
                     </Upload>
                 </Form.Item>
 
-                <Form.Item label="Title">
+                <Form.Item label="Title" name='title'>
                     <Input />
                 </Form.Item>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Category" className='w-1/2'>
-                        <Select className='pr-2'>
+                    <Form.Item label="Category" className='w-1/2' name='category'>
+                        <Select className='pr-2' >
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo1">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
                         </Select>
                     </Form.Item>
-                    <Form.Item label="Season" className='w-1/2'>
+                    <Form.Item label="Season" className='w-1/2' name='season'>
                         <Select className='pl-2'>
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
+                            <Select.Option value="demo5">Demo</Select.Option>
                         </Select>
                     </Form.Item>
                 </div>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Land Type" className='w-1/2'>
-                        <Select className='pr-2'>
+                    <Form.Item label="Land Type" className='w-1/2' name='land_type'>
+                        <Select className='pr-2' >
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo1">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
                         </Select>
                     </Form.Item>
-                    <Form.Item label="Soil Type" className='w-1/2'>
-                        <Select className='pl-2'>
+                    <Form.Item label="Soil Type" className='w-1/2' name='soil_type'>
+                        <Select className='pl-2' >
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
+                            <Select.Option value="demo5">Demo</Select.Option>
                         </Select>
                     </Form.Item>
                 </div>
-                <Form.Item label="Sowing Time" className='w-full'>
+                <Form.Item label="Sowing Time" className='w-full' name='sowing_time' >
                     <RangePicker className='w-full' />
                 </Form.Item>
-                <Form.Item label="Planting Time" className='w-full'>
-                    <RangePicker className='w-full' />
+                <Form.Item label="Planting Time" className="w-full" name="planting_time">
+                    <DatePicker.RangePicker className="w-full" onChange={handlePlantingTimeChange} />
                 </Form.Item>
 
-                <Form.Item label="Seedling Amount" className='w-full'>
+                <Form.Item label="Seedling Amount" className='w-full' name='seedling_amount'>
                     <InputNumber className='w-full' />
                 </Form.Item>
+
                 <p className='font-semibold text-lg mb-2'>Distance</p>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Plant to Plant" className='w-1/2'>
+                    <Form.Item label="Plant to Plant" className='w-1/2' name={['distance', 'plant_to_plant']}>
                         <div className='pr-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
-                    <Form.Item label="Row to Row" className='w-1/2'>
+                    <Form.Item label="Row to Row" className='w-1/2' name={['distance', 'row_to_row']}>
                         <div className='pl-2'>
                             <InputNumber className='w-full' />
                         </div>
@@ -169,39 +186,37 @@ const FormDisabledDemo = () => {
                 </div>
 
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Disease" className='w-1/2'>
-                        <Select className='pr-2'>
+                    <Form.Item label="Disease" className='w-1/2' name='disease'>
+                        <Select className='pr-2' >
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
+                            <Select.Option value="demo5">Demo</Select.Option>
                         </Select>
                     </Form.Item>
-                    <Form.Item label="Insects" className='w-1/2'>
-                        <Select className='pl-2'>
+                    <Form.Item label="Insects" className='w-1/2' name='insects'>
+                        <Select className='pl-2' >
                             <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
-                            <Select.Option value="demo">Demo</Select.Option>
+                            <Select.Option value="demo2">Demo</Select.Option>
+                            <Select.Option value="demo3">Demo</Select.Option>
+                            <Select.Option value="demo4">Demo</Select.Option>
+                            <Select.Option value="demo5">Demo</Select.Option>
                         </Select>
                     </Form.Item>
                 </div>
-                <Form.Item label="Harvest Time" className='w-full'>
+                <Form.Item label="Harvest Time" className='w-full' name='harvest_time'>
                     <RangePicker className='w-full' />
                 </Form.Item>
-                <Form.Item label="" className='w-full'>
-                    <RangePicker className='w-full' />
-                </Form.Item>
+
                 <p className='font-semibold text-lg mb-2'>Crop Production</p>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Maximum" className='w-1/2'>
+                    <Form.Item label="Maximum" className='w-1/2' name={['crop_production', 'max']}>
                         <div className='pr-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
-                    <Form.Item label="Minimum" className='w-1/2'>
+                    <Form.Item label="Minimum" className='w-1/2' name={['crop_production', 'min']}>
                         <div className='pl-2'>
                             <InputNumber className='w-full' />
                         </div>
@@ -209,12 +224,12 @@ const FormDisabledDemo = () => {
                 </div>
                 <p className='font-semibold text-lg mb-2'>Crop Price</p>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Maximum" className='w-1/2'>
+                    <Form.Item label="Maximum" className='w-1/2' name={['crop_prices', 'max']}>
                         <div className='pr-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
-                    <Form.Item label="Minimum" className='w-1/2'>
+                    <Form.Item label="Minimum" className='w-1/2' name={['crop_prices', 'min']}>
                         <div className='pl-2'>
                             <InputNumber className='w-full' />
                         </div>
@@ -222,12 +237,12 @@ const FormDisabledDemo = () => {
                 </div>
                 <p className='font-semibold text-lg mb-2'>Total Cost</p>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Maximum" className='w-1/2'>
+                    <Form.Item label="Maximum" className='w-1/2' name={['cost', 'max']}>
                         <div className='pr-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
-                    <Form.Item label="Minimum" className='w-1/2'>
+                    <Form.Item label="Minimum" className='w-1/2' name={['cost', 'min']}>
                         <div className='pl-2'>
                             <InputNumber className='w-full' />
                         </div>
@@ -235,20 +250,20 @@ const FormDisabledDemo = () => {
                 </div>
                 <p className='font-semibold text-lg mb-2'>NET Profit</p>
                 <div className='flex justify-between w-full'>
-                    <Form.Item label="Maximum" className='w-1/2'>
+                    <Form.Item label="Maximum" className='w-1/2' name={['profit', 'max']}>
                         <div className='pr-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
-                    <Form.Item label="Minimum" className='w-1/2'>
+                    <Form.Item label="Minimum" className='w-1/2' name={['profit', 'min']}>
                         <div className='pl-2'>
                             <InputNumber className='w-full' />
                         </div>
                     </Form.Item>
                 </div>
 
-                <Form.Item label="Discription">
-                    <TextArea cols={6} />
+                <Form.Item label="Description">
+                    <TextArea cols={6} name='description' />
                 </Form.Item>
 
 
